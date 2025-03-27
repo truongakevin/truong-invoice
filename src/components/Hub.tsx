98930eed180c48960065abf0de8035e6c5d181f3
Kevin Truong <42168530+kevinoli@users.noreply.github.com>
2025-03-26 20:25:39 -0600

    hub styles

--- src/components/Hub.tsx
+++ src/components/Hub.tsx
@@ -5,34 +5,35 @@ import ContactsPage from './contacts/ContactsPage';
 // import Payments from './Payments';
 
 const Hub: React.FC = () => {
-  const [activeTab, setActiveTab] = useState('invoices');
+  const [activeTab, setActiveTab] = useState('Contacts');
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="fex flex-col min-h-screen w-full items-start justify-start m-0 p-0 border-0 shadow-none rounded-none bg-neutral-50">
       {/* Header */}
-      <div className="bg-white shadow-lg px-20 pt-12 pb-8 flex justify-between items-end">
-        <h1 className="text-5xl font-bold">TruongInvoice</h1>
-        <div className="">
-          {['invoices', 'contacts', 'estimates', 'payments'].map((tab) => (
-            <button
-              key={tab}
-              onClick={() => setActiveTab(tab)}
-              className={`p-4 pb-0 text-3xl font-semibold rounded-md transition text-black ${
-                activeTab === tab ? '' : 'hover:scale-110'
-              }`}
-            >
-              {tab.charAt(0).toUpperCase() + tab.slice(1)}
-            </button>
-          ))}
+      <div className="flex flex-row p-16 pb-0 shadow-lg justify-between items-end bg-neutral-200">
+        <div className='flex flex-row gap-8'>
+            {['Home', 'Invoices', 'Contacts', 'Estimates', 'Payments', 'Settings'].map((tab) => (
+              <div
+                key={tab}
+                onClick={() => setActiveTab(tab)}
+                className={`pb-4 text-3xl font-semibold transition ${
+                  activeTab === tab ? 'border-b-4 border-neutral-800' : 'hover:scale-110'}`}
+              >
+                {tab}
+              </div>
+            ))}
         </div>
+        <h1 className="pb-6 text-5xl font-bold">TruongInvoice</h1>
       </div>
-
+      
       {/* Tab Content */}
-      <div className="py-4 px-24 bg-gray-100">
-        {activeTab === 'invoices' && <InvoicesPage />}
-        {activeTab === 'contacts' && <ContactsPage />}
+      <div className="px-10">
+        {/* {activeTab === 'Home' && <Home />} */}
+        {activeTab === 'Invoices' && <InvoicesPage />}
+        {activeTab === 'Contacts' && <ContactsPage />}
         {/* {activeTab === 'estimates' && <Estimates />}
         {activeTab === 'payments' && <Payments />} */}
+        {/* {activeTab === 'Settings' && <Settings />} */}
       </div>
     </div>
   );
